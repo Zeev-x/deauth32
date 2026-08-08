7ec559baface2d640cb846bd217d4a4f9990d90b
Reyette Roxylious Atelier <87901027+Zeev-x@users.noreply.github.com>
2026-08-08 09:10:54 +0700

    Add files via upload

---
+++ src/deauth32.cpp
@@ -0,0 +1,10 @@
+#include "deauth.h"
+#include "definitions.h"
+
+void deauth23() {
+  enable_deauth();
+}
+
+void deauth32_loop() {
+  deauth_loop();
+}
---
+++ src/deauth32.h
@@ -0,0 +1,7 @@
+#ifndef DEAUTH32_H
+#define DEAUTH32_H
+
+void deauthrey();
+void deauthrey_loop();
+
+#endif
